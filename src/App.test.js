cbecd0135b4f33262634b5f6357ef74c798f9f1e
kkdev211 <kkdev211@users.noreply.github.com>
2020-03-22 16:27:11 -0400

    set up tests outline

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,22 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, fireEvent } from "@testing-library/react";
 import App from "./App";
+import ContactForm from "./components/ContactForm";
 
 test("renders App without crashing", () => {
   render(<App />);
 });
+//test one first name string length
+test('first name', () => {
+
+})
+//test two last name string length
+test('last name', () => {
+  
+})
+// test three email string
+test('email', () => {
+  
+})
+
+//test for onCLick button
